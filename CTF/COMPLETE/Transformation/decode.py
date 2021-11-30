c1be459a30fba399440e25f9df3b6d9c41ca3494
WickedRider <95270086+WickedRider@users.noreply.github.com>
2021-11-30 21:15:01 +0000

    Delete CTF/COMPLETE directory

--- CTF/COMPLETE/Transformation/decode.py
+++
@@ -1,5 +0,0 @@
-enconded_string = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸弰摤捤㤷慽"
-for i in range(len(enconded_string)):
-    print(chr(ord(enconded_string[i])>>8), end="")
-    print(chr((ord(enconded_string[i]))-((ord(enconded_string[i])>>8)<<8)), end="")
-print(" ")
