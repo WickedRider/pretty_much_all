195a4a86f307c94a12ecdf2785d68e390e872621
Hugo Priva <idkdoi35@protonmail.com>
2022-02-27 20:04:12 +0000

    MATLAB

---
+++ MatLab/is_even.m
@@ -0,0 +1,10 @@
+function y = is_even( x )
+%IS_EVEN Summary of this function goes here
+%   Detailed explanation goes here
+
+y = ~mod(x,2);
+
+% 1 -> even
+% 0 -> odd
+end
+
---
+++ MatLab/my_sinh.m
@@ -0,0 +1,6 @@
+function  y  = my_sinh( x )
+%COMMENTS
+
+y = (exp(x) - exp(-x))/2;
+
+end
